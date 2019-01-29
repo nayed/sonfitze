d63a33ebb021a2688e444eef99dfd4187226c234
Nayed Saïd Ali <nayed.saidali@gmail.com>
2019-01-29 22:26:16 +0100

    Updates

---
+++ precache-manifest.ec8e3de70a30d98bc82f61022eea91be.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "76d344f08412543c74ccd1fb2493d607",
+    "url": "/sonfitze/static/media/github-logo.76d344f0.svg"
+  },
+  {
+    "revision": "5e69441ebfc6236b8397",
+    "url": "/sonfitze/static/js/runtime~main.5e69441e.js"
+  },
+  {
+    "revision": "0aeb9d6e92920e401566",
+    "url": "/sonfitze/static/js/main.0aeb9d6e.chunk.js"
+  },
+  {
+    "revision": "c5134f49941656210e55",
+    "url": "/sonfitze/static/js/1.c5134f49.chunk.js"
+  },
+  {
+    "revision": "41d0ed59eebae8e4e5592029809dec1e",
+    "url": "/sonfitze/index.html"
+  }
+];
